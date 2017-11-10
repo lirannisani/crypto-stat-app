d50012c5f68e5b48427bdf726ebd916160a5a436
liran.n <liran.n@jellybtn.com>
2017-11-10 17:39:18 +0200

    protfolio real data

--- CryptoStat/src/pages/protfolio/protfolio.ts
+++ CryptoStat/src/pages/protfolio/protfolio.ts
@@ -14,54 +14,90 @@ export class ProtfolioPage {
     coins: any = [];
     protfolio: any = [];
 
-    changeIn24Hour = 7.3;
-    totalValue = 5000.22;
+    changeIn24Hour: number = 0;
+    totalValue: number = 0;
 
     constructor(public modalCtrl: ModalController, public apiProvider: ApiProvider) {
         this.coins = this.apiProvider.coinsDic;
+        var bitcoinDetails = this.coins["Bitcoin"];
+        var ethDetails = this.coins["Ethereum"];
+        var bitcoinCashDetails = this.coins["Bitcoin Cash"];
+
+        console.log("bitcoin cash:" + bitcoinCashDetails);
+        var iotaDetails = this.coins["IOTA"];
+
+        console.log("details: " + bitcoinDetails);
         var bitcoin = {
-            price: 6892.23,
+            price: bitcoinDetails.price,
             amount: 2,
-            name: "Bitcoin",
-            symbol: "BTC",
-            change24: 4.65
+            name: bitcoinDetails.name,
+            symbol: bitcoinDetails.symbol,
+            change24: bitcoinDetails.change24
         };
 
         var eth = {
-            price: 311.66,
-            amount: 4,
-            name: "ethereum",
-            symbol: "ETH",
-            change24: 1.79
+            price: ethDetails.price,
+            amount: 1,
+            name: ethDetails.name,
+            symbol: ethDetails.symbol,
+            change24: ethDetails.change24
+        };
+
+        var bitcoinCash = {
+            price: bitcoinCashDetails.price,
+            amount: 1,
+            name: bitcoinCashDetails.name,
+            symbol: bitcoinCashDetails.symbol,
+            change24: bitcoinCashDetails.change24
         };
 
         var iot = {
-            price: 0.487904,
-            amount: 4,
-            name: "iota",
-            symbol: "IOT",
-            change24: -1.3
+            price: iotaDetails.price,
+            amount: 1,
+            name: iotaDetails.name,
+            symbol: iotaDetails.symbol,
+            change24: iotaDetails.change24
         };
 
 
         this.protfolio.push(bitcoin);
         this.protfolio.push(eth);
+        this.protfolio.push(bitcoinCash);
         this.protfolio.push(iot);
+
+        this.CalcTotalAndChange();
+    }
+
+    CalcTotalAndChange() {
+        for (var coinInProtfolio of this.protfolio) {
+            console.log("price: " + coinInProtfolio.price);
+            console.log("priceFixed: " + coinInProtfolio.price.toFixed(2));
+
+            this.totalValue += coinInProtfolio.price * coinInProtfolio.amount;
+            console.log("change24: " + coinInProtfolio.change24.toFixed(2));
+            console.log("change24 before add: " + this.changeIn24Hour);
+            this.changeIn24Hour += parseFloat(coinInProtfolio.change24);
+
+            console.log("Total change24:" + this.changeIn24Hour);
+        }
+
+        this.changeIn24Hour = parseFloat(Number(this.changeIn24Hour).toFixed(2));
+        this.totalValue = parseFloat(Number(this.totalValue).toFixed(2));
     }
 
     openModal() {
 
-        let modal = this.modalCtrl.create(ModalContentPage, { coins: this.coins });
+        //let modal = this.modalCtrl.create(ModalContentPage, { coins: this.coins });
 
-        // Getting data from the modal:
-        modal.onDidDismiss(data => {
-            console.log('MODAL DATA', data);
-            this.protfolio.push(data);
-            console.log('after MODAL DATA', data);
+        //// Getting data from the modal:
+        //modal.onDidDismiss(data => {
+        //    console.log('MODAL DATA', data);
+        //    this.protfolio.push(data);
+        //    console.log('after MODAL DATA', data);
 
-        });
+        //});
 
-        modal.present();
+        //modal.present();
     }
 
     onLink(url: string) {
