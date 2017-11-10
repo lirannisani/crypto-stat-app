4c8564f605b07ed5f1e4eff5ccc1204cec3372a3
liran.n <liran.n@jellybtn.com>
2017-11-10 16:06:50 +0200

    protfolio v1

--- CryptoStat/src/pages/protfolio/protfolio.ts
+++ CryptoStat/src/pages/protfolio/protfolio.ts
@@ -14,8 +14,39 @@ export class ProtfolioPage {
     coins: any = [];
     protfolio: any = [];
 
+    changeIn24Hour = 7.3;
+    totalValue = 5000.22;
+
     constructor(public modalCtrl: ModalController, public apiProvider: ApiProvider) {
         this.coins = this.apiProvider.coinsDic;
+        var bitcoin = {
+            price: 6892.23,
+            amount: 2,
+            name: "Bitcoin",
+            symbol: "BTC",
+            change24: 4.65
+        };
+
+        var eth = {
+            price: 311.66,
+            amount: 4,
+            name: "ethereum",
+            symbol: "ETH",
+            change24: 1.79
+        };
+
+        var iot = {
+            price: 0.487904,
+            amount: 4,
+            name: "iota",
+            symbol: "IOT",
+            change24: -1.3
+        };
+
+
+        this.protfolio.push(bitcoin);
+        this.protfolio.push(eth);
+        this.protfolio.push(iot);
     }
 
     openModal() {
